e1880c04c39aa124969eaa5c8aa9bf547d4dbfa4
Mongrel401 <32886893+Mongrel401@users.noreply.github.com>
2017-10-25 16:01:22 +0800

    Change

--- doc_process/execute.php
+++ doc_process/execute.php
@@ -9,7 +9,7 @@ $dochist_type = 1;
 $dochist_not_active = 0;
 $dochist_active = 1;
 
-$sampak = "UPDATE `documents_history` SET `dochist_active`= '$dochist_not_active' WHERE `document_id` = '$document_id';";
+$sampak = "UPDATE `documents_history` SET `dochist_active` = '$dochist_not_active' WHERE `document_id` = '$document_id';";
 $sampak .= "INSERT INTO `documents_history`(`document_id`, `dochist_timestamp`, `dochist_type`, `department_id`, `user_id`, `dochist_active`, `dochist_remarks`) VALUES ('$document_id',CURRENT_TIMESTAMP(),'$dochist_type','$dept_id','$user_id','$dochist_active','');";
 
 if ($con->multi_query($sampak) === TRUE) {
